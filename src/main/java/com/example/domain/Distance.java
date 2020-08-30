87a69e91ff0d39cc5ce67a68c0ba87002f3b0509
Kostennik <kostennik@gmail.com>
2020-08-30 13:15:03 +0200

    added information to method

--- src/main/java/com/example/domain/Distance.java
+++ src/main/java/com/example/domain/Distance.java
@@ -1,9 +1,17 @@
 package com.example.domain;
 
-public class Distance {
-    private double distanceBetween;
+import org.springframework.stereotype.Component;
 
-    public static double distanceMeasurement(Coordinate beginCoordinate, Coordinate targetCoordinate) {
+@Component
+public class Distance {
+
+    /**
+     * measures a distance between current position and target position
+     * @param beginCoordinate is begin position (current)
+     * @param targetCoordinate is target position
+     * @return distance in meters
+     */
+    public float distanceMeasurement(Coordinate beginCoordinate, Coordinate targetCoordinate) {
         double earthRadius = 6371000; //meters
         double dLat = Math.toRadians(targetCoordinate.getLatitude() - beginCoordinate.getLatitude());
         double dLng = Math.toRadians(targetCoordinate.getLongitude() - beginCoordinate.getLongitude());
