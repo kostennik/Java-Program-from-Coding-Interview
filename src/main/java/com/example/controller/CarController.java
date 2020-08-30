5b41257cb077a807922ca5db216cbcf5541514cb
Kostennik <kostennik@gmail.com>
2020-08-30 17:45:16 +0200

    added CarController

---
+++ src/main/java/com/example/controller/CarController.java
@@ -0,0 +1,27 @@
+package com.example.controller;
+
+import com.example.domain.Car;
+import com.example.domain.Coordinate;
+import com.example.repository.CarRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CarController {
+
+    private final CarRepository repository;
+
+    public CarController(CarRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/getCars/{lat}/{lon}/{meters}")
+    public List<Car> getAll(@PathVariable double lat, @PathVariable double lon, @PathVariable int meters){
+        List<Car> allByCoordinates = repository.findAllByCoordinates(new Coordinate(lat, lon), meters);
+        System.out.println(allByCoordinates);
+        return allByCoordinates;
+    }
+}
