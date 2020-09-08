41f69f3b0cc417b716433f7d5680c3275652abc1
Kostennik <kostennik@gmail.com>
2020-09-08 18:52:07 +0200

    added implementation auto load all Cars from file to memory after application run

--- src/main/java/com/example/RunApplication.java
+++ src/main/java/com/example/RunApplication.java
@@ -1,11 +1,21 @@
 package com.example;
 
+import com.example.repository.CarRepository;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RunApplication {
     public static void main(String[] args) {
         SpringApplication.run(RunApplication.class, args);
+    }
+
+    //load all Cars from file to memory
+    @Bean
+    public CommandLineRunner dataLoader(CarRepository repo) {
+        return args -> repo.loadAllCarsInMemory();
     }
 }
