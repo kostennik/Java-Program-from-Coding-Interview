abe647a6d443b62d22d3cfa7fea6551037261685
Kostennik <kostennik@gmail.com>
2020-09-09 11:31:37 +0200

    implementation multi-threading

--- src/main/java/com/example/controller/CarController.java
+++ src/main/java/com/example/controller/CarController.java
@@ -4,19 +4,24 @@ import com.example.Memory;
 import com.example.domain.Car;
 import com.example.domain.Coordinate;
 import com.example.repository.CarRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @RestController
 public class CarController {
 
     private final CarRepository repository;
+    private List<Car> cars = new ArrayList<>();
+    private Map<Integer, Car> carMap = new HashMap<>();
 
     public CarController(CarRepository repository) {
         this.repository = repository;
@@ -24,11 +29,22 @@ public class CarController {
 
     @GetMapping("/getCars/{lat}/{lon}/{meters}")
     public ResponseEntity<Map<Integer, Car>> getByCoordinate(@PathVariable double lat, @PathVariable double lon, @PathVariable int meters) {
-        List<Car> cars = repository.findCarsByCoordinatesAndDistance(new Coordinate(lat, lon), meters);
-        Map<Integer, Car> carMap = new HashMap<>();
-        for (int i = 0; i < cars.size(); i++) {
-            carMap.put(i, cars.get(i));
+
+        Thread userThread = new Thread(() -> {
+            cars = repository.findCarsByCoordinatesAndDistance(new Coordinate(lat, lon), meters);
+            for (int i = 0; i < cars.size(); i++) {
+                carMap.put(i, cars.get(i));
+            }
+        });
+
+        userThread.start();
+        try {
+            userThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        log.info("new thread {} was created", userThread.getName());
+        System.out.println(carMap.toString());
         return ResponseEntity.ok().body(carMap);
     }
 }
