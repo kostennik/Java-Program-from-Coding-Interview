429077857436af8805212652ac2b04af9b76cc0f
Kostennik <kostennik@gmail.com>
2020-09-11 11:43:28 +0200

    added tests of methods, documentation

--- src/test/java/com/example/ApiDocumentationJUnit5IntegrationTest.java
+++ src/test/java/com/example/ApiDocumentationJUnit5IntegrationTest.java
@@ -1,52 +1,102 @@
 package com.example;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.context.WebApplicationContext;
+import org.yecht.Data;
 
-import static org.springframework.restdocs.headers.HeaderDocumentation.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-
+@WebAppConfiguration
 @ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 @SpringBootTest
 public class ApiDocumentationJUnit5IntegrationTest {
+    private static final String CLIENT_ID = "clientOne";
+    private static final String CLIENT_SECRET = "secret";
+    private static final String CONTENT_TYPE = "application/json;charset=UTF-8";
+    private static final String USERNAME = "myUsername";
+    private static final String PASSWORD = "myPassword";
+    private String accessToken;
+
+    @Autowired
+    private FilterChainProxy springSecurityFilterChain;
 
     private MockMvc mockMvc;
 
     @BeforeEach
     public void setUp(WebApplicationContext webApplicationContext,
-                      RestDocumentationContextProvider restDocumentation) {
+                      RestDocumentationContextProvider restDocumentation) throws Exception {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
                 .apply(documentationConfiguration(restDocumentation))
                 .alwaysDo(document("{method-name}",
                         preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint())))
+                .addFilter(springSecurityFilterChain)
                 .build();
 
+        //get Access Token
+        final MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "password");
+        params.add("username", USERNAME);
+        params.add("password", PASSWORD);
+
+        ResultActions result = mockMvc.perform(post("/oauth/token")
+                .params(params)
+                .with(httpBasic(CLIENT_ID, CLIENT_SECRET))
+                .accept(CONTENT_TYPE))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(CONTENT_TYPE));
+
+
+        String resultString = result.andReturn().getResponse().getContentAsString();
+
+        JacksonJsonParser jsonParser = new JacksonJsonParser();
+        accessToken = jsonParser.parseMap(resultString).get("access_token").toString();
     }
 
     @Test
-    public void getCarsByLocation() throws Exception {
-        this.mockMvc.perform(get("/getCars/{latitude}/{longitude}/{distance}", 53.90, 20.88, 50)
-                .header("Authorization", "Bearer <your_token>")
-                .accept(MediaType.APPLICATION_JSON))
+    public void getToken() throws Exception {
+        final MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "password");
+        params.add("username", USERNAME);
+        params.add("password", PASSWORD);
+
+        this.mockMvc.perform(post("/oauth/token")
+                .params(params)
+                .with(httpBasic(CLIENT_ID, CLIENT_SECRET))
+                .accept(CONTENT_TYPE))
                 .andExpect(status().isOk())
-                .andDo(document("headers",
-                        responseHeaders(
-                                headerWithName("Cars-found").description("2"))));
+                .andExpect(content().contentType(CONTENT_TYPE));
 
-
+    }
+    
+    @Test
+    public void getCarsByLocation() throws Exception {
+        this.mockMvc.perform(get("/getCars/{latitude}/{longitude}/{distance}", 53.90, 20.88, 100)
+                .header("Authorization", "Bearer " + accessToken)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 }
