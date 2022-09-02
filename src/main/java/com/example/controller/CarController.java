e0a33b41a91a7bf7a4ae04a049c9194030120b6a
Andrzey Kostennik <andrey.kostennik@programisci.eu>
2022-09-02 15:40:00 +0200

    fix getting car controller, added new endpoint load csv, changed request size

--- src/main/java/com/example/controller/CarController.java
+++ src/main/java/com/example/controller/CarController.java
@@ -2,49 +2,30 @@ package com.example.controller;
 
 import com.example.domain.Car;
 import com.example.domain.Coordinate;
-import com.example.repository.CarRepository;
+import com.example.service.CarService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @RestController
+@RequestMapping(produces = "application/json")
 public class CarController {
+    private final CarService carService;
 
-    private final CarRepository repository;
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
 
-    public CarController(CarRepository repository) {
-        this.repository = repository;
+    @PostMapping("/loadCsv")
+    public void loadCsv(@RequestParam("file") MultipartFile file) {
+        carService.loadCsv(file);
     }
 
     @GetMapping("/getCars/{lat}/{lon}/{meters}")
-    public ResponseEntity<Map<Integer, Car>> getCarsByCoordinateAndDistance(@PathVariable double lat, @PathVariable double lon, @PathVariable int meters) {
-        Map<Integer, Car> carsMap = new HashMap<>();
-
-        Thread userThread = new Thread(() -> {
-            List<Car> cars = repository.findCarsByCoordinatesAndDistance(new Coordinate(lat, lon), meters);
-            for (int i = 0; i < cars.size(); i++) {
-                carsMap.put(i, cars.get(i));
-            }
-        });
-
-        userThread.start();
-
-        try {
-            userThread.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        log.info("new thread {} was created", userThread.getName());
-
-        return ResponseEntity.ok().body(carsMap);
+    public Iterable<Car> getCarsByCoordinateAndDistance(@PathVariable double lat, @PathVariable double lon, @PathVariable int meters) {
+        return carService.findByCoordinatesAndDistance(new Coordinate(lat, lon), meters);
     }
 }
